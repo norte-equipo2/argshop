85d423c5b88b98812c4e899a4c4acd9a165d88e9
gabriel taglioretti <gabrieltaglioretti@gmail.com>
2020-06-07 16:07:00 -0300

    Solucion Inicial

---
+++ SolutionLPPA/HTML.WebSite/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTML.WebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "La página de descripción de su aplicación.";
+            return View();
+        }
+        
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Su página de contacto.";
+            return View();
+        }
+    }
+}
