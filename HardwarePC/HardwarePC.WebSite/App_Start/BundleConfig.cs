9cc2f7a393319518ddfa488d0512d29fc08149e3
Sebastian Favio Romero <SebastianFavio.Romero@Alumnos.uai.edu.ar>
2020-06-14 19:00:26 -0300

    Banner, Contenido, Footer

--- HardwarePC/HardwarePC.WebSite/App_Start/BundleConfig.cs
+++ HardwarePC/HardwarePC.WebSite/App_Start/BundleConfig.cs
@@ -19,12 +19,7 @@ namespace HardwarePC.WebSite
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
         }
     }
 }
