2fe0521e96df3a627c9101a5eab1e9ac0187497e
Sebastian Favio Romero <SebastianFavio.Romero@Alumnos.uai.edu.ar>
2020-06-15 21:55:10 -0300

    Agrego capas Data y Service. Agrego ProductoController.

---
+++ HardwarePC/HardwarePC.Data/Services/IProductoData.cs
@@ -0,0 +1,14 @@
+﻿using HardwarePC.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwarePC.Data.Services
+{
+    public interface IProductoData
+    {
+        IEnumerable<Producto> GetAll();
+    }
+}
